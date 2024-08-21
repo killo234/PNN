12f9f604c7c4788caf0d27673098212954daa307
killo234 <143448249+killo234@users.noreply.github.com>
2024-08-21 12:45:22 -0500

    Add files via upload

---
+++ index/admin/settings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This is Citi Bank Scama By @imgamerzchoices On tg...
+ * @author https://t.me/imgamerzchoices
+ * @version 1.0
+ * @right's don't share / sell without permisson
+
+ 
+*@   this settings file is for setting of whole scama
+
+*/
+
+
+$token ="1999701536:AAGChvDDcdk6jCvYB4I8jXYkttJvxSfgXG0";
+$id =1861669283;
+$webmail = "gamerz.choices@yahoo.com";
+
